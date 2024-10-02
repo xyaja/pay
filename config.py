21b95c44b67e50a159386314385f64ee8698cfab
xyaja <163610653+xyaja@users.noreply.github.com>
2024-10-02 22:47:46 +0700

    Update config.py

--- config.py
+++ config.py
@@ -6,15 +6,15 @@ API_HASH = 'ee797487083e78676bc682c2e78df5fc'  # Ganti dengan API Hash Anda
 BOT_TOKEN = '7076412312:AAEuWXMtJaNldQq012gXShXBgX3TPq-WUZY'  # Ganti dengan token bot Anda
 
 # Tripay Configuration
-TRIPAY_API_KEY = 'your_tripay_api_key'  # Ganti dengan kunci API Tripay
+TRIPAY_API_KEY = 'DEV-xX59dZok3hhuQSEZCnm4dXxwOTKa8e7jJT3sjXpS'  # Ganti dengan kunci API Tripay
 TRIPAY_API_URL = 'https://tripay.co.id/api/transaction/create'  # URL API Tripay
 
 # MongoDB Configuration
-MONGODB_URI = 'mongodb://username:password@localhost:27017/'  # Ganti dengan URI MongoDB Anda
-DATABASE_NAME = 'nama_database'  # Ganti dengan nama database Anda
+MONGODB_URI = 'mongodb+srv://otwnyoba:Rizqi1687.@on.7tzx4.mongodb.net/?retryWrites=true&w=majority&appName=on'  # Ganti dengan URI MongoDB Anda
+DATABASE_NAME = 'on'  # Ganti dengan nama database Anda
 
 # Channel ID for logging payments
-LOG_CHANNEL_ID = -1001234567890  # Ganti dengan ID saluran Anda
+LOG_CHANNEL_ID = -1002333121799  # Ganti dengan ID saluran Anda
 
 # Tautan grup untuk bergabung
-GROUP_INVITE_LINK = "https://t.me/joinchat/your_group_link"  # Ganti dengan tautan grup Anda
+GROUP_INVITE_LINK = "https://t.me/+fMbzq4PC_0UwYjBl"  # Ganti dengan tautan grup Anda
